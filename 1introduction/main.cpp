a86a73daa37178524939e7ae1f50f9067af85ea3
pos4n <posan.gc@g.austincc.edu>
2024-06-03 10:56:01 -0500

    Testing changes still

--- 1introduction/main.cpp
+++ 1introduction/main.cpp
@@ -6,9 +6,13 @@
 //
 
 #include <iostream>
+using namespace std;
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello WORLD\n";
+    // HELLO PROFESSOR
+    // HOW TO COMMIT
+    
+    cout << "Hello WORLD\n";
     return 0;
 }
