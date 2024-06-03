10444a8c4db1ab620d6180bc3b587785aa9d8222
pos4n <posan.gc@g.austincc.edu>
2024-06-02 21:55:46 -0500

    Initial Commit

---
+++ 1introduction/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1introduction
+//
+//  Created by Posan Gc on 6/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
